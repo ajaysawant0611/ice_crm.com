3b88c8b324b7c5789b48b447cc19699a8abe2380
ajaysawant0611 <ajaysawant5592@gmail.com>
2023-04-28 13:26:29 +0530

    done

--- src/test/java/com/ice_hrm_automation/TestingExamples/AddEmpOrangeHrm.java
+++ src/test/java/com/ice_hrm_automation/TestingExamples/AddEmpOrangeHrm.java
@@ -3,17 +3,20 @@ package com.ice_hrm_automation.TestingExamples;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import ice_hrm.com.utility.Baseclass;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.testng.annotations.AfterClass;
 
-public class AddEmpOrangeHrm extends BaseClassforOrangehrm {
+public class AddEmpOrangeHrm extends Baseclass{
 	String firstName="Dom";
 	 String middleName="S";
 	 
 	 String lastName="Torreto";
 	 String empId="9399";
-	 
+	 String nickname="Domnic";
 	 
   @BeforeClass
   public void beforeClass() throws Exception ,NoSuchElementException{
@@ -31,13 +34,15 @@ public class AddEmpOrangeHrm extends BaseClassforOrangehrm {
 	 // click(By.xpath(""));//click on employees
 	  //click(By.xpath(""));//click on  admin employee
 	  
-	 // driver.findElement(By.xpath("//button[@type='submit']"));    //div[@id="app"]/div[1]/div[1]/header/div[1]/div[1]/span/h6
+	
+	// driver.findElement(By.xpath("//button[@type='submit']"));    //div[@id="app"]/div[1]/div[1]/header/div[1]/div[1]/span/h6
+	
   }
  @Test
 public void test() {
 	  System.out.println("This is a Test Method");
 	  
-	  click(By.xpath("//div[@class=\"oxd-topbar-body\"]/descendant::a[2]"));
+	 click(By.xpath("//div[@class=\"oxd-topbar-body\"]/descendant::a[2]"));
 	  
 		 
 	  //enter emp id
@@ -50,6 +55,10 @@ public void test() {
 	  enterText(By.name("lastName"),lastName);
 	 
 	click(By.xpath("//button[@type=\"submit\"]"));
+	
+	//enter nickname
+	//enterText(By.xpath("//label[@class=\"oxd-label\"]/following::div/input[1]"),nickname);
+	//click(By.xpath("//button[@type=\"submit\"][1]"));
 }
   @AfterClass
   public void afterClass() {
--- src/test/java/orangehrmdataprovider/loginpage/EmpTest.java
+++ src/test/java/orangehrmdataprovider/loginpage/EmpTest.java
@@ -1,5 +1,42 @@
 package orangehrmdataprovider.loginpage;
 
-public class EmpTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+public class EmpTest {
+		public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		  ChromeOptions co= new ChromeOptions();
+		  co.addArguments("--remote-allow-origins=*");
+		  System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		WebDriver  driver = new ChromeDriver(co);
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).submit();
+		Thread.sleep(5000);
+		String dashboardText=driver.findElement(By.xpath("//h6[text()='Dashboard']")).getText();
+		if(dashboardText.equals("Dashboard"))
+		{
+			System.out.println("successfully landed on the homepage of an application");
+		}
+		else
+		{
+			throw new Exception("failed to logine to application");
+		}
+		driver.findElement(By.xpath("//li[@class=\"oxd-main-menu-item-wrapper\"][1]")).click();
+		//click on admin 
+		//click on add
+		driver.findElement(By.xpath("//i[@class=\"oxd-icon bi-plus oxd-button-icon\"]")).click();
+		//click on add button
+		driver.findElement(By.xpath("//input[@placeholder=\"Type for hints...\"]")).sendKeys("Tom");
+		//emp name		
+		driver.findElement(By.xpath("//div[@class=\"oxd-input-group__label-wrapper\"]/following::input[1]")).sendKeys("Jerry");
+		
+
+}
 }
